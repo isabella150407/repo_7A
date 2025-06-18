67ea1d157d3c7653f02095c07739ae83ce1effac
isabella150407 <portilloisa862@gmail.com>
2025-06-18 12:00:32 -0300

    ejercicio14.java

---
+++ ejercicio14.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PuestoTorneoSwitch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la posición obtenida en el torneo (en número): ");
+        int puesto = scanner.nextInt();
+
+        switch (puesto) {
+            case 1:
+                System.out.println("El primero obtiene la medalla de oro.");
+                break;
+            case 2:
+                System.out.println("El segundo obtiene la medalla de plata.");
+                break;
+            case 3:
+                System.out.println("El tercero obtiene la medalla de bronce.");
+                break;
+            default:
+                System.out.println("Siga participando.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
