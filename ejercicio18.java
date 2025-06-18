0dde24fa2005b804568d4d69d2d51e8a81675c82
isabella150407 <portilloisa862@gmail.com>
2025-06-18 12:18:13 -0300

    ejercicio18.java

---
+++ ejercicio18.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TablasMultiplicar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese hasta qué número desea ver las tablas de multiplicar: ");
+        int limite = scanner.nextInt();
+
+        // Ciclos for anidados
+        for (int i = 1; i <= limite; i++) {
+            System.out.println("Tabla del " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println(); // línea en blanco entre tablas
+        }
+
+        scanner.close();
+    }
+}
