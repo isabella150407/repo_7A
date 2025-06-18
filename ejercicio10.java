544e7c546f27e36cfcc072cfba0f626651e2a1cb
isabella150407 <portilloisa862@gmail.com>
2025-06-18 11:45:01 -0300

    ejercicio10.java

---
+++ ejercicio10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MayorDeTresAnd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número entero: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Ingrese el segundo número entero: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Ingrese el tercer número entero: ");
+        int num3 = scanner.nextInt();
+
+        if (num1 >= num2 && num1 >= num3) {
+            System.out.println("El mayor de los tres números es: " + num1);
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println("El mayor de los tres números es: " + num2);
+        } else {
+            System.out.println("El mayor de los tres números es: " + num3);
+        }
+
+        scanner.close();
+    }
+}
