e233a6da0f0bf68e227518a0c1b046b7a829c3d9
isabella150407 <portilloisa862@gmail.com>
2025-06-18 12:20:22 -0300

    ejercicio19.java

---
+++ ejercicio19.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class NumerosAlAzar {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int contador = 0;
+        int suma = 0;
+
+        while (contador < 10) {
+            int numero = random.nextInt(100) + 1; // Genera números del 1 al 100
+            System.out.println("Número " + (contador + 1) + ": " + numero);
+            suma += numero;
+            contador++;
+        }
+
+        double promedio = suma / 10.0;
+        System.out.println("Suma total: " + suma);
+        System.out.println("Promedio: " + promedio);
+    }
+}
