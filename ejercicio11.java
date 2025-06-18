a05e3c6b4db45f9cdeb47616d048e65537792641
isabella150407 <portilloisa862@gmail.com>
2025-06-18 11:48:51 -0300

    ejercicio11.java

---
+++ ejercicio11.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class VocalONo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un carácter: ");
+        String entrada = scanner.next();
+
+        if (entrada.length() != 1) {
+            System.out.println("Error, solo un carácter a la vez.");
+        } else {
+            char letra = Character.toLowerCase(entrada.charAt(0));
+            if (esVocal(letra)) {
+                System.out.println("El carácter '" + letra + "' es una vocal.");
+            } else {
+                System.out.println("El carácter '" + letra + "' no es una vocal.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    // Función que determina si un carácter es vocal
+    public static boolean esVocal(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
