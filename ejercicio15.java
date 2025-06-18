7c4d44e4bb763d3e73e9541d1c97635d64e870db
isabella150407 <portilloisa862@gmail.com>
2025-06-18 12:03:30 -0300

    ejercicio15.java

---
+++ ejercicio15.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CaracteristicasAuto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la clase del auto (a, b o c): ");
+        char clase = scanner.next().toLowerCase().charAt(0); // Acepta mayúsculas o minúsculas
+
+        switch (clase) {
+            case 'a':
+                System.out.println("Clase A: 4 ruedas y un motor.");
+                break;
+            case 'b':
+                System.out.println("Clase B: 4 ruedas, un motor, cierre centralizado y aire.");
+                break;
+            case 'c':
+                System.out.println("Clase C: 4 ruedas, un motor, cierre centralizado, aire y airbag.");
+                break;
+            default:
+                System.out.println("Clase no válida. Ingrese 'a', 'b' o 'c'.");
+                break;
+        }
+
+        scanner.close();
+    }
+}
