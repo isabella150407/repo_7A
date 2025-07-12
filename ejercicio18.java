7ca5c821a29c0fd7f8c087c06d7ae5cfbf0d8c5d
isabella150407 <portilloisa862@gmail.com>
2025-07-12 14:17:09 -0300

    ejercicio18.java

--- ejercicio18.java
+++ ejercicio18.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TablasMultiplicar {
+public class ejercicio18 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
