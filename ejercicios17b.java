303c429cb3e7528d088b22e71b5e000a70cd4086
isabella150407 <portilloisa862@gmail.com>
2025-07-12 14:38:59 -0300

    ejercicios17b.java

--- ejercicios17b.java
+++ ejercicios17b.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EvaluacionSinIf {
+public class ejercicios17b{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
