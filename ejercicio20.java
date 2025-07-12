c638b0389d1b6f851f2ae482ae438137a3e8ced1
isabella150407 <portilloisa862@gmail.com>
2025-07-12 14:18:39 -0300

    ejercicio20.java

--- ejercicio20.java
+++ ejercicio20.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class NumerosAlAzarDoWhile {
+public class ejercicio20 {
     public static void main(String[] args) {
         Random random = new Random();
         int contador = 0;
