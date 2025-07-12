b880f7d055de187ed61568e86d39bb5eaf5b76e5
isabella150407 <portilloisa862@gmail.com>
2025-07-12 14:29:34 -0300

    ejercicio2.java

--- ejercicio2.java
+++ ejercicio2.java
@@ -5,7 +5,7 @@ public class ejercicio2 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ingrese un número entero: ");
-        int numero = scanner.nextInt();  // La variable es de tipo int
+        int numero = scanner.nextInt(); 
 
         if (numero % 2 == 0) {
             System.out.println("El número " + numero + " es PAR.");
