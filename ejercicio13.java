fd1f8703e7bba067bbe4e42ff9d058309b19d78a
isabella150407 <portilloisa862@gmail.com>
2025-07-12 14:11:05 -0300

    ejercicio13.java

--- ejercicio13.java
+++ ejercicio13.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DiasDelMesSwitch {
+public class ejercicio13 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
