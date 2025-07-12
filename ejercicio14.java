41512b92e335aea0541fbda08e5b7f4985d0e276
isabella150407 <portilloisa862@gmail.com>
2025-07-12 14:11:51 -0300

    ejercicio14.java

--- ejercicio14.java
+++ ejercicio14.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PuestoTorneoSwitch {
+public class ejercicio14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
