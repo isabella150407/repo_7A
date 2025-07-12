62c3c3ed98cce8c332b0a718975a756e99f77bbb
isabella150407 <portilloisa862@gmail.com>
2025-07-12 14:06:56 -0300

    ejercicio5.java

--- ejercicio5.java
+++ ejercicio5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PosicionTorneo {
+public class ejercicio5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
