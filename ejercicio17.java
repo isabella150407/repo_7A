49bce1e0bfb2296892dad96ae210a8e7c1586829
isabella150407 <portilloisa862@gmail.com>
2025-07-12 14:15:22 -0300

    ejercicio17.java

--- ejercicio17.java
+++ ejercicio17.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EvaluacionConIf {
+public class ejercicio17 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
