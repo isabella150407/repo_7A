8caf647996b0282dc2790211ce206f317b83bc8c
isabella150407 <portilloisa862@gmail.com>
2025-07-12 14:34:56 -0300

    ejercicio12.java

--- ejercicio12.java
+++ ejercicio12.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 
-public class ejercicio12 {
+public class ejercicio12{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Ingrese un número entero: ");
         int numero = scanner.nextInt();
 
-        // Verificamos si el número está fuera del rango usando OR
         if (numero < 1 || numero > 36) {
             System.out.println("El número " + numero + " está fuera de rango.");
         } else {
-            // Usamos AND para determinar la docena
+            
             if (numero >= 1 && numero <= 12) {
                 System.out.println("El número " + numero + " pertenece a la primera docena.");
             } else if (numero >= 13 && numero <= 24) {
